c70a747d4b6cefb28d5925e2549410733abe4cfb
Jan Wilmake <jan@wilmake.com>
2025-01-22 15:42:15 +0100

    improvements

--- set-secrets.sh
+++ set-secrets.sh
@@ -1,6 +1,31 @@
-# Export local vars to GitHub secrets
-cat .dev.vars | while read line; do
-  KEY=$(echo $line | cut -d'=' -f1)
-  VALUE=$(echo $line | cut -d'=' -f2)
-  gh secret set "$KEY" -b "$VALUE"
-done
+#!/bin/bash
+
+# Process each line in .dev.vars
+while IFS= read -r line || [[ -n "$line" ]]; do
+  # Trim leading/trailing whitespace
+  trimmed_line="${line#"${line%%[![:space:]]*}"}"
+  trimmed_line="${trimmed_line%"${trimmed_line##*[![:space:]]}"}"
+
+  # Skip empty lines and comments
+  if [[ -z "$trimmed_line" || "$trimmed_line" == '#'* ]]; then
+    continue
+  fi
+
+  # Split key and value at first '='
+  key="${trimmed_line%%=*}"
+  value="${trimmed_line#*=}"
+
+  # Trim whitespace from key and value
+  key="${key%"${key##*[![:space:]]}"}"
+  key="${key#"${key%%[![:space:]]*}"}"
+  value="${value%"${value##*[![:space:]]}"}"
+  value="${value#"${value%%[![:space:]]*}"}"
+
+  # Set secret if key is valid
+  if [[ -n "$key" ]]; then
+    # echo "$key to $value"
+    gh secret set "$key" -b "$value"
+  else
+    echo "Warning: Invalid line '${line}' - missing key"
+  fi
+done < .dev.vars
